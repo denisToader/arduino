fae2ade59aa7076b5a12538a868928a41efd983a
denisToader <toader.denis2308@gmail.com>
2017-05-12 20:06:31 +0300

    Inca cateva fisiere

---
+++ src/sketch_may12a/sketch_may12a.ino
@@ -0,0 +1,26 @@
+#include "AccelStepper.h"
+
+int step_pin = 3;
+int dir_pin = 2;
+int enable_pin = 4;
+
+AccelStepper motor(AccelStepper:: DRIVER, step_pin, dir_pin);
+
+void setup() {
+  // put your setup code here, to run once:
+  motor.maxSpeed(100);
+  motor.setMaxSpeed(100);
+  motor.setAcceleration(100);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(motor.distanceToGo() == 0) {
+    int dir = rand()%2;
+    if(dir ==0)
+      motor.move(100);
+     else
+      motor.move(-100);
+  }
+  motor.run();
+}
